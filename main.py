c19404655475f139b1625642f573ba5c7c666025
Tayyeba <official.tayyeba@gmail.com>
2025-07-11 14:59:13 +0100

    Phase 1 completed. Code runs correcty and on run displays a window when hand's are visible to camera. it also displays nodes on fingers and palms

---
+++ main.py
@@ -0,0 +1,37 @@
+import cv2
+import mediapipe as mp
+
+# initialise mediapipe
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp.draw = mp.solutions.drawing_utils
+ 
+# start the webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, frame = cap.read()
+    if not success:
+        break
+
+    # flip and convert to RGB
+    frame = cv2.flip(frame, 1)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # process the frames
+    results = hands.process(rgb)
+
+    # if hands are detected
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp.draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+        # show the frame
+        cv2.imshow("Gesture Mouse", frame)
+
+        # quit on "q" press
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
